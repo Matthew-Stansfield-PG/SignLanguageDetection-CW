1477538e88eec4d9861b8837e6e24f225bd3539f
Jakub Wochniak <UnlivedDisc03@gmail.com>
2025-03-04 14:40:43 +0000

    test2

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import torch
 from torch.xpu import device
 from ultralytics import YOLO
-
 import torch
 import torch.nn as nn
 from torch import optim
@@ -9,13 +8,14 @@ from torch.utils.data import Dataset, DataLoader
 
 print(torch.__version__)
 print(torch.version.cuda)
-device = torch.device('cpu')
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-print(device)
-# print(torch.cuda.get_device_name(0))
-#
+print(torch.cuda.is_available())
+
+
+#print(torch.cuda.get_device_name(0))
+
 # model = YOLO("yolov8n.pt")
 # data_path = "C:/Users/Unliv/PycharmProjects/SignLanguageDetection-CW/data/dataset/data.yaml"
 #
-# results = model.train(data=data_path, epochs=1, device=0)
+# results = model.train(data=data_path, epochs=100)
+# results = model.val()
+# results = model("C:/Users/Unliv/PycharmProjects/SignLanguageDetection-CW/data/dataset/images/test/C19_jpg.rf.0add6365e398b8d188f428bac97eb868.jpg")
