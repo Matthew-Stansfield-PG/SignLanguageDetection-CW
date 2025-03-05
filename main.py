ba1035dc80835da0ee261b8e609e9ae10c271f9d
Jakub Wochniak <UnlivedDisc03@gmail.com>
2025-03-05 15:55:35 +0000

    wandb working, oushing so we can use it on hyperion

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from ultralytics import YOLO
 import torch
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 from logger import Logger
 
-import torch
-from ultralytics import YOLO
-
-
-
-def train(logger):
+def train():
     print(torch.__version__)
     print(torch.version.cuda)
     print(torch.cuda.is_available())
@@ -16,12 +13,23 @@ def train(logger):
     model = YOLO("yolov8n.pt")
     data_path = "C:/Users/Unliv/PycharmProjects/SignLanguageDetection-CW/data/dataset/data.yaml"
 
-    results = model.train(data=data_path, epochs=3, device="cpu")
+    add_wandb_callback(model, enable_model_checkpointing=True)
+    results = model.train(data=data_path, epochs=3, device=0)
+    print(results)
+    print("--------")
+
+    model.val()
+    model(["C:/Users/Unliv/PycharmProjects/SignLanguageDetection-CW/data/dataset/images/test/C19_jpg.rf.0add6365e398b8d188f428bac97eb868.jpg"])
+
+
 
 def main():
-    wandb_logger = Logger(f"Sign_language_recognition_model", project='INM705-CW')#name of model, project name
-    logger = wandb_logger.get_logger()
-    train(logger)
+    wandb.login(key="4dbce09ff68bb778f03c435cfda8289ee37a28e2")
+    wandb.init(project="INM705-CW", name="Sign_language_recognition_model", job_type="training")
+    #wandb_logger = Logger(f"Sign_language_recognition_model", project='INM705-CW')#name of model, project name
+    #logger = wandb_logger.get_logger()
+    train()
+    wandb.finish()
 
 if __name__ == '__main__':
     main()
