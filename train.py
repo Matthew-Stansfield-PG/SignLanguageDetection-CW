a4c7279dad7893dcea144a7c378abfabfc8ef4d9
Jakub Wochniak <UnlivedDisc03@gmail.com>
2025-03-27 14:22:27 +0000

    renamed main to train

---
+++ train.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import torch
+import random
+from torch import nn
+import torchvision
+import torchvision.transforms as transforms
+import math
+import matplotlib.pyplot as plt
+import time
+import multiprocessing
+
+from dataset import get_data
+
+#if main required to avoid erroring and allow multiprocessing
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+
+    # chooses device for training, cuda better
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    print("Current device: " + str(device))
+
+    # makes results reproducible
+    manualSeed = 42
+    print("Random Seed: ", manualSeed)
+    random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
+    torch.use_deterministic_algorithms(True)
+
+    #establishes the training set tensor from dataset.py
+    training_set = get_data()
+
+
