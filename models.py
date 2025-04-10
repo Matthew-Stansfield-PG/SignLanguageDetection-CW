1dd9900f33acbd3becbe8eec8a491101d9ee364d
Matthew-Stansfield-PG <matthew.stansfield@city.ac.uk>
2025-04-10 12:13:04 +0100

    Add files via upload
    Updated the training and model files

--- models.py
+++ models.py
@@ -12,29 +12,23 @@ class Generator(nn.Module):
         self.ngf = config['image_size'] #feature_map_size_generator
         self.noc = config['noc']
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-
-            nn.ConvTranspose2d(self.nz, self.ngf * 8, 4, 1, 0, bias=False),
-            #nn.BatchNorm2d(self.ngf * 8),
+            nn.ConvTranspose2d(self.nz, self.ngf * 8, 4, 1, 0, bias=False),  # 4x4
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(self.ngf * 4),
+            nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False),  # 8x8
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(self.ngf * 2),
+            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),  # 16x16
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False),
-            #nn.BatchNorm2d(self.ngf),
+            nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False),  # 32x32
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            #nn.ConvTranspose2d(self.ngf, self.nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(self.ngf, self.ngf // 2, 4, 2, 1, bias=False),  # 64x64
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.ngf // 2, self.ngf // 4, 4, 2, 1, bias=False),  # 128x128
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.ngf // 4, self.noc, 4, 2, 1, bias=False),  # 256x256
             nn.Tanh()
-
-            # state size. 3 x 64 x 64
         )
+
+
 
     def forward(self, input):
         return self.main(input)
@@ -83,5 +77,4 @@ class Discriminator(nn.Module):
 
     def forward(self, input):
         features = self.main(input)
-        return features
-
+        return features.view(input.size(0), -1).mean(dim=1, keepdim=True)
--- train.py
+++ train.py
@@ -1,36 +1,94 @@
 import os
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import torch
 import random
-from torch import nn
-import torchvision
+from torch import nn, optim
 import torchvision.transforms as transforms
-import math
-import matplotlib.pyplot as plt
 import time
 import multiprocessing
-
 from dataset import get_data
-
-#if main required to avoid erroring and allow multiprocessing
+from models import Discriminator, Generator
+from logger import Logger
+import torchvision.utils as vutils
+import os
+import yaml
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-
-    # chooses device for training, cuda better
-    device = torch.device('cpu')
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-    print("Current device: " + str(device))
-
-    # makes results reproducible
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    print("Current device:", device)
     manualSeed = 42
-    print("Random Seed: ", manualSeed)
+    print("Random Seed:", manualSeed)
     random.seed(manualSeed)
     torch.manual_seed(manualSeed)
     torch.use_deterministic_algorithms(True)
-
-    #establishes the training set tensor from dataset.py
+    num_epochs = 500
+    batch_size = 10
+    generator_input_size = config['nz']
+    learning_rate = 0.0002
+    beta1 = 0.5
+    # Get dataset and DataLoader
     training_set = get_data()
-
-
+    train_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True)
+    # Initialize models
+    generator = Generator().to(device)
+    discriminator = Discriminator().to(device)
+    # Loss and optimizers
+    loss_function = nn.BCELoss()
+    optimizer_generator = optim.Adam(generator.parameters(), lr=learning_rate, betas=(beta1, 0.999))
+    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(beta1, 0.999))
+    # Logger
+    logger = Logger('gan-training').get_logger()
+    # Create output directory
+    output_dir = "output_images"
+    os.makedirs(output_dir, exist_ok=True)
+    # Fixed noise for evaluation (e.g., 64 samples)
+    fixed_noise = torch.randn(64, generator_input_size, 1, 1).to(device)
+    start = time.time()
+    for epoch in range(num_epochs + 1):
+        print("Running epoch number: ",epoch)
+        for n, (real_samples, _) in enumerate(train_loader):
+            print(n)
+            real_samples = real_samples.to(device)
+            real_samples_labels = torch.ones((real_samples.size(0), 1)).to(device)
+            # Generate fake samples
+            latent_space_samples = torch.randn((real_samples.size(0), generator_input_size, 1, 1)).to(device)
+            generated_samples = generator(latent_space_samples)
+            generated_samples_labels = torch.zeros((real_samples.size(0), 1)).to(device)
+            # Train Discriminator
+            discriminator.zero_grad()
+            all_samples = torch.cat((real_samples, generated_samples))
+            all_labels = torch.cat((real_samples_labels, generated_samples_labels))
+            output_discriminator = discriminator(all_samples).view(-1, 1)
+            loss_discriminator = loss_function(output_discriminator, all_labels)
+            loss_discriminator.backward()
+            optimizer_discriminator.step()
+            # Train Generator
+            generator.zero_grad()
+            latent_space_samples = torch.randn((real_samples.size(0), generator_input_size, 1, 1)).to(device)
+            generated_samples = generator(latent_space_samples)
+            output_discriminator_generated = discriminator(generated_samples).view(-1, 1)
+            loss_generator = loss_function(output_discriminator_generated, real_samples_labels)
+            loss_generator.backward()
+            optimizer_generator.step()
+        print("epoch training ended")
+        print(epoch % 5)
+        if epoch % 5 == 0:
+            print("Time taken for 5 epochs: {:.2f}s".format(time.time() - start))
+            start = time.time()
+            print(f"Epoch {epoch} - Loss D: {loss_discriminator.item():.4f}, Loss G: {loss_generator.item():.4f}")
+            logger.log({
+                'epoch': epoch,
+                'loss_discriminator': loss_discriminator.item(),
+                'loss_generator': loss_generator.item()
+            })
+            # Save generated images
+            generator.eval()
+            with torch.no_grad():
+                fake_images = generator(fixed_noise)
+                fake_images = (fake_images + 1) / 2  # Rescale from [-1, 1] to [0, 1]
+                vutils.save_image(fake_images, os.path.join(output_dir, f"epoch_{epoch}.png"), nrow=8, normalize=True)
+            generator.train()
+        print("epoch ended")
