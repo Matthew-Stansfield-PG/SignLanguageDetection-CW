054ec65017685de044989bdeb2615616023eb90d
Jakub Wochniak <UnlivedDisc03@gmail.com>
2025-04-15 16:47:13 +0100

    updated logger to track our configs

--- logger.py
+++ logger.py
@@ -1,16 +1,19 @@
 import wandb
-#
-#
-class Logger:
+import yaml
 
+class Logger:
     def __init__(self, experiment_name, logger_name='logger', project='INM705-COURSEWORK-Tuning'):
+        with open('config.yaml', 'r') as f:
+            config = yaml.safe_load(f)
         logger_name = f'{logger_name}-{experiment_name}'
-        logger = wandb.init(project=project, name=logger_name)
+        logger = wandb.init(project=project, name=logger_name, config=config)
         self.logger = logger
         return
 
     def get_logger(self):
         return self.logger
+
+logger=Logger(experiment_name='test')
 
 # class Logger:
 #
