e9daa227ba37a5d8c8948f955c70eff7eef32369
Matthew Stansfield <matthew.stansfield@city.ac.uk>
2025-04-20 10:13:13 +0100

    Added progress SAGAN style Self-Attention, changed from batch norm to instancenorm in the discriminator

--- models.py
+++ models.py
@@ -1,18 +1,53 @@
-import torch.nn as nn
+import os
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 import torch
+import torch.nn as nn
 import yaml
+
+current_epoch = 0
+
+class ProgressiveSAGANSelfAttention(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.query = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.key = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.value = nn.Conv2d(in_channels, in_channels, 1)
+        self.scale = in_channels ** -0.5
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.norm = nn.InstanceNorm2d(in_channels, affine=True)
+
+    def forward(self, x):
+        if current_epoch < 150:
+            return x
+
+        alpha = min(1.0, max(0.0, (current_epoch - 150) / 150))
+
+        B, C, H, W = x.size()
+        q = self.query(x).view(B, -1, H * W).permute(0, 2, 1)
+        k = self.key(x).view(B, -1, H * W)
+        v = self.value(x).view(B, -1, H * W)
+
+        attn = torch.softmax(torch.bmm(q, k) * self.scale, dim=-1)
+        out = torch.bmm(v, attn.permute(0, 2, 1)).view(B, C, H, W)
+
+        out = x + alpha * self.gamma * out
+        return self.norm(out)
+
 
 class Generator(nn.Module):
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
-        nz=config['nz']
-        ngf=config['ngf']
-        nc=config['nc']
-    def __init__(self, nz=nz, ngf=ngf, nc=3):
+        nz = config['nz']
+        ngf = config['ngf']
+        nc = config['nc']
+
+    def __init__(self, nz=nz, ngf=ngf, nc=nc, n_classes=3):
         super(Generator, self).__init__()
+        self.nz = nz
+        self.label_embedding = nn.Embedding(n_classes, 10)
+
         self.main = nn.Sequential(
-            # Input: Z latent vector
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz + 10, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
 
@@ -21,6 +56,7 @@ class Generator(nn.Module):
             nn.ReLU(True),
 
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            ProgressiveSAGANSelfAttention(ngf * 2),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
 
@@ -29,39 +65,50 @@ class Generator(nn.Module):
             nn.ReLU(True),
 
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
-            nn.Tanh()  # Output is normalized [-1, 1]
+            nn.Tanh()
         )
 
-    def forward(self, input):
-        return self.main(input)
+    def forward(self, input, labels):
+        label_emb = self.label_embedding(labels).unsqueeze(2).unsqueeze(3)
+        x = torch.cat((input, label_emb), dim=1)
+        return self.main(x)
+
 
 class Discriminator(nn.Module):
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
-        nc=config['nz']
-        ndf=config['ndf']
-    def __init__(self, nc=3, ndf=64):
+        nc = config['nc']
+        ndf = config['ndf']
+
+    def __init__(self, nc=nc, ndf=ndf, n_classes=3, embed_dim=1):
         super(Discriminator, self).__init__()
+        self.embed_dim = embed_dim
+        self.embed = nn.Embedding(n_classes, self.embed_dim)
+
         self.main = nn.Sequential(
-            # Input: Image
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(nc + self.embed_dim, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
 
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.InstanceNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.2),
 
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            ProgressiveSAGANSelfAttention(ndf * 4),
+            nn.InstanceNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.2),
 
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.InstanceNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
 
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-            # No Sigmoid!
         )
 
-    def forward(self, input):
+    def forward(self, input, labels):
+        label_map = self.embed(labels).unsqueeze(2).unsqueeze(3)
+        label_map = label_map.expand(-1, -1, 64, 64)
+        input = torch.cat([input, label_map], dim=1)
         return self.main(input)
--- trainRedone.py
+++ trainRedone.py
@@ -11,11 +11,59 @@ import time
 import math
 import multiprocessing
 import wandb
-
 from dataset import get_data
-from models import Discriminator, Generator
+from models import Discriminator, Generator, current_epoch
 from logger import Logger
+from torchmetrics.image.fid import FrechetInceptionDistance
+import torchvision.transforms.functional as TF
 
+fid_metric = FrechetInceptionDistance(feature=2048).to('cuda' if torch.cuda.is_available() else 'cpu')
+resize_for_fid = transforms.Resize((299, 299))
+def to_uint8(tensor):
+    tensor = (tensor * 127.5 + 127.5).clamp(0, 255)
+    return tensor.to(torch.uint8)
+
+def compute_fid_score(generator, real_images, labels, device, nz):
+    fid_metric = FrechetInceptionDistance(feature=2048).to(device)
+
+    generator.eval()
+    with torch.no_grad():
+        batch_size = real_images.size(0)
+        noise = torch.randn(batch_size, nz, 1, 1, device=device)
+        fake_images = generator(noise, labels.to(device))
+
+    fake_uint8 = to_uint8(fake_images.cpu())
+    real_uint8 = to_uint8(real_images.cpu())
+
+    fid_metric.update(real_uint8.to(device), real=True)
+    fid_metric.update(fake_uint8.to(device), real=False)
+
+    return fid_metric.compute().item()
+def compute_gradient_penalty(D, real_samples, fake_samples, labels):
+    alpha = torch.rand(real_samples.size(0), 1, 1, 1, device=real_samples.device)
+    interpolates = alpha * real_samples + ((1 - alpha) * fake_samples)
+    interpolates.requires_grad_(True)
+
+    d_interpolates = D(interpolates, labels)
+
+    gradients = torch.autograd.grad(
+        outputs=d_interpolates,
+        inputs=interpolates,
+        grad_outputs=torch.ones_like(d_interpolates),
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True
+    )[0]
+
+    gradients = gradients.view(gradients.size(0), -1)
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+def d_loss(real_scores, fake_scores, gp, λ=10):
+    return -(torch.mean(real_scores) - torch.mean(fake_scores)) + λ * gp
+
+def g_loss(fake_scores):
+    return -torch.mean(fake_scores)
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -24,7 +72,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
@@ -57,7 +104,7 @@ if __name__ == '__main__':
         transforms.Normalize((-0.5, -0.5, -0.5), (1, 1, 1))
     ])
 
-    for real_samples, real_label in train_loader:
+    for real_samples, real_labels in train_loader:
         for i in range(batch_size):
             ax = plt.subplot(math.ceil(math.sqrt(batch_size)), math.ceil(math.sqrt(batch_size)), i + 1)
             sample = denormalize(real_samples[i])
@@ -65,62 +112,90 @@ if __name__ == '__main__':
             plt.xticks([]); plt.yticks([])
         break
 
-    generator = Generator(config['nz'], 64, 3).to(device)
-    discriminator = Discriminator(3, 64).to(device)
+    n_classes = 3
+    generator = Generator(config['nz'], 64, 3, n_classes).to(device)
+    discriminator = Discriminator(config['nc'], 64, n_classes).to(device)
     generator.apply(weights_init)
     discriminator.apply(weights_init)
 
-
     loss_function = nn.BCEWithLogitsLoss()
-    optimizer_generator = optim.Adam(generator.parameters(), lr=learning_rate, betas=(beta1, 0.999))
-    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(beta1, 0.999))
+    optimizer_generator = optim.Adam(generator.parameters(), lr=learning_rate, betas=(config["beta1"], config["beta2"]))
+    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(config["beta1"], config["beta2"]))
 
-    logger = Logger('gan-training').get_logger()
-
+    logger = Logger('Spectral-Norm-Added-ChangedConfig').get_logger()
 
     output_dir = "output_images"
     os.makedirs(output_dir, exist_ok=True)
     fixed_noise = torch.randn(64, generator_input_size, 1, 1).to(device)
 
+    max_grad_norm = 10.0  #
+
     for epoch in range(num_epochs + 1):
+        current_epoch = epoch
         start = time.time()
-        for n, (real_samples, _) in enumerate(train_loader):
+        for n, (real_samples, real_labels) in enumerate(train_loader):
             real_samples = real_samples.to(device)
+            real_labels = real_labels.to(device)
             b_size = real_samples.size(0)
 
-            # Smoothing: real=0.9, fake=0.0
-            real_labels = torch.full((b_size,), 0.9, dtype=torch.float, device=device)
-            fake_labels = torch.zeros(b_size, dtype=torch.float, device=device)
-
-            # Train Discriminator
             discriminator.zero_grad()
-
-            output_real = discriminator(real_samples).view(-1)
-            loss_real = loss_function(output_real, real_labels)
-
-            noise = torch.randn(b_size, generator_input_size, 1, 1, device=device)
-            fake_samples = generator(noise)
-            output_fake = discriminator(fake_samples.detach()).view(-1)
-            loss_fake = loss_function(output_fake, fake_labels)
-
-            loss_discriminator = loss_real + loss_fake
-            loss_discriminator.backward()
-            optimizer_discriminator.step()
-
             generator.zero_grad()
-            output = discriminator(fake_samples).view(-1)
-            loss_generator = loss_function(output, real_labels)
-            loss_generator.backward()
-            optimizer_generator.step()
+
+            class_losses_D = {}
+            class_losses_G = {}
+
+            unique_labels = torch.unique(real_labels)
+
+            loss_discriminator = 0.0
+            loss_generator = 0.0
+
+            for label_class in unique_labels:
+                class_mask = real_labels == label_class
+                class_indices = class_mask.nonzero(as_tuple=True)[0]
+
+                real_class = real_samples[class_indices]
+                labels_class = real_labels[class_indices]
+
+                # === Generate Fake ===
+                noise_class = torch.randn(real_class.size(0), generator_input_size, 1, 1, device=device)
+                fake_class = generator(noise_class, labels_class)
+
+                # === Discriminator ===
+                discriminator.zero_grad()
+                real_scores = discriminator(real_class, labels_class).view(-1)
+                fake_scores = discriminator(fake_class.detach(), labels_class).view(-1)
+                gp = compute_gradient_penalty(discriminator, real_class.data, fake_class.data, labels_class)
+                loss_D = d_loss(real_scores, fake_scores, gp)
+                loss_D.backward(retain_graph=True)
+                torch.nn.utils.clip_grad_norm_(discriminator.parameters(), max_grad_norm)  # ⬅️ Clip discriminator
+                optimizer_discriminator.step()
+                loss_discriminator += loss_D.item()
+                class_losses_D[int(label_class)] = loss_D.item()
+                generator.zero_grad()
+                fake_scores_G = discriminator(fake_class, labels_class).view(-1)
+                loss_G = g_loss(fake_scores_G)
+                loss_G.backward(retain_graph=True)
+
+                torch.nn.utils.clip_grad_norm_(generator.parameters(), max_grad_norm)  # ⬅️ Clip generator
+                optimizer_generator.step()
+
+                loss_generator += loss_G.item()
+                class_losses_G[int(label_class)] = loss_G.item()
 
             if n % 50 == 0:
-                print(f"Epoch [{epoch}/{num_epochs}] Batch {n}/{len(train_loader)} \
-                      Loss D: {loss_discriminator.item():.4f}, loss G: {loss_generator.item():.4f}")
+                print(f"Epoch [{epoch}/{num_epochs}] Batch {n}/{len(train_loader)} "
+                      f"Loss D: {loss_discriminator:.4f}, Loss G: {loss_generator:.4f}")
 
         logger.log({
             'epoch': epoch,
-            'loss_discriminator': loss_discriminator.item(),
-            'loss_generator': loss_generator.item()
+            'loss_discriminator_total': loss_discriminator,
+            'loss_generator_total': loss_generator,
+            'loss_D_cat': class_losses_D.get(0, 0),
+            'loss_D_dog': class_losses_D.get(1, 0),
+            'loss_D_wild': class_losses_D.get(2, 0),
+            'loss_G_cat': class_losses_G.get(0, 0),
+            'loss_G_dog': class_losses_G.get(1, 0),
+            'loss_G_wild': class_losses_G.get(2, 0)
         }, step=epoch)
 
         print("Time for epoch:", round(time.time() - start, 2), "seconds")
@@ -128,13 +203,22 @@ if __name__ == '__main__':
         if epoch % 5 == 0:
             generator.eval()
             with torch.no_grad():
-                fake_images = generator(fixed_noise).detach().cpu()
+                sample_labels = [0]*21 + [1]*21 + [2]*22
+                sample_labels = torch.tensor(sample_labels, dtype=torch.long, device=device)
+                fake_images = generator(fixed_noise, sample_labels).detach().cpu()
                 fake_images = (fake_images + 1) / 2
                 vutils.save_image(fake_images, f"{output_dir}/epoch_{epoch}.png", nrow=8, normalize=True)
                 logger.log({
-                    "DCGAN Generated Images": wandb.Image(vutils.make_grid(fake_images, nrow=8).permute(1,2,0).numpy(), caption=f"Epoch: {epoch}")
-                }, step=epoch)#assembles images in a grid, permute swaps order of channels, height, width to h, w, c for wandb's structure
+                    "DCGAN Generated Images": wandb.Image(
+                        vutils.make_grid(fake_images, nrow=8).permute(1, 2, 0).numpy(), caption=f"Epoch: {epoch}")
+                }, step=epoch)
             generator.train()
+            real_batch, real_labels = next(iter(train_loader))
+            real_batch = real_batch[:64].to(device)
+            real_labels = real_labels[:64].to(device)
+            fid = compute_fid_score(generator, real_samples, real_labels, device, config['nz'])
+            logger.log({'epoch': epoch,"FID Score": fid}, step=epoch)
+            print(f"FID Score at epoch {epoch}: {fid:.4f}")
             print("saving and logging images...")
 
     torch.save(generator.state_dict(), "animalImageGAN.pt")
