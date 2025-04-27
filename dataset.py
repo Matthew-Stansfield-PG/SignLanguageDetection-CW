a494b9f750018a2f149e2496655e72f7cc9601f9
Matthew Stansfield <matthew.stansfield@city.ac.uk>
2025-04-27 13:18:09 +0100

    Final tweaks

--- dataset.py
+++ dataset.py
@@ -3,66 +3,48 @@ import os
 import torchvision.transforms as transforms
 import torchvision
 import yaml
+from torch.utils.data import Dataset
+from PIL import Image
 
-def get_data():
 
+def get_data(transform=None, image_resolution=64):
     cwd = os.getcwd()
-
-    #we decided to combine val and train for more training images as validation set is not required for dcgans
-
-    # train_file_names = []
-    # for root, dirs, filenames in os.walk(cwd + '/afhq/train'):
-    #     for filename in filenames:
-    #         train_file_names.append(filename)
-    # print("Number of Training Data: " +str(len(train_file_names)))
-    #
-    # #length of val images
-    # val_file_names = []
-    # for root, dirs, filenames in os.walk(cwd + '/afhq/val'):
-    #     for filename in filenames:
-    #         val_file_names.append(filename)
-    # print("Number of Validation Data: " +str(len(val_file_names)))
-    #
-    # #confirms no overlaps in files, val images are unique to train images
-    # matches = list(set(train_file_names) & set(val_file_names))
-    # print(len(matches))
-
+    # Count training samples
     train_file_names = []
     for root, dirs, filenames in os.walk(cwd + '/dataAnimals/train'):
         for filename in filenames:
-            train_file_names.append(filename)
-    print("Total training data: " +str(len(train_file_names)))
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                train_file_names.append(filename)
+    print(f"Total training data: {len(train_file_names)}")
 
+    # Load configuration file
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
-    image_size =  config['image_size']
-    root_path = cwd + "/dataAnimals/train/"
 
-    #makes all images 256x256 and transforms them to a tensor
-    transform = transforms.Compose([
-        transforms.Resize((image_size, image_size)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # Extract image_size from config, or default to the given image_resolution argument
+    image_size = image_resolution  # Using the dynamic resolution passed in
+    root_path = os.path.join(cwd, "dataAnimals/train/")
 
-    training_set = torchvision.datasets.ImageFolder(root=root_path, transform=transform)
+    # Enhanced transforms with augmentation
+    if transform is None:
+        transform = transforms.Compose([
+            transforms.Resize(int(image_size * 1.125)),  # Resize to slightly larger than target resolution
+            transforms.RandomCrop(image_size),  # Crop to the target resolution
+            transforms.RandomHorizontalFlip(p=0.5),  # Random horizontal flip with 50% probability
+            transforms.ToTensor(),  # Convert to tensor
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # Normalize to [-1, 1]
+        ])
 
-    #counts how many of each label appear, takes a while to run so itll be left commented out.
-    # cats=0 #5653
-    # dogs=0 #5239
-    # wild=0 #5238
-    # for i in range(len(training_set)):
-    #     image, label = training_set[i]
-    #     if label == 0:
-    #         cats += 1
-    #     elif label == 1:
-    #         dogs += 1
-    #     else:
-    #         wild += 1
-    #
-    # print("Training image distribution: Cats = "+str(cats)+", Dogs = "+str(dogs)+", Wild = "+str(wild))
+    # Create the dataset with the applied transformations
+    training_set = torchvision.datasets.ImageFolder(
+        root=root_path,
+        transform=transform,
+        is_valid_file=lambda x: x.lower().endswith(('.png', '.jpg', '.jpeg'))
+    )
 
+    # Print class distribution
+    class_counts = pd.Series([label for _, label in training_set]).value_counts()
+    print("\nClass distribution:")
+    print(class_counts.to_string())
 
     return training_set
-
-
-#train_set = get_data()
